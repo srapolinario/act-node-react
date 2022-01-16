973c78bb73811a26e73c9fadff96f23c0bce0039
Samuel Cardoso <samuelcardosodf@gmail.com>
2022-01-16 11:01:42 -0300

    incluido ex. variavel tipo undefined

---
+++ aulas/aula1/ex4.ts
@@ -0,0 +1,14 @@
+/*
+ * Declarando variáveis e entendendo tipos de dados
+ * No escopo abaixo existe o código:
+ * "export default {}"
+ * No contexto atual estamos exportando por padrão o que vier na frente,
+ * no caso {}.
+ *
+ * Faça a declaração de uma variável do tipo undefined
+ * Troque "{}" pelo nome dado a variável
+ *
+ */
+const teste = undefined;
+
+export default teste;
--- tests/aula1.spec.ts
+++ tests/aula1.spec.ts
@@ -1,6 +1,7 @@
 import ex1 from "../aulas/aula1/ex1";
 import ex2 from "../aulas/aula1/ex2";
 import ex3 from "../aulas/aula1/ex3";
+import ex4 from "../aulas/aula1/ex4";
 
 describe("Aula 1 - Conhecendo variáveis e seus tipos", () => {
     it("Variável do tipo string", () => {
@@ -17,5 +18,9 @@ describe("Aula 1 - Conhecendo variáveis e seus tipos", () => {
         console.log("Variável declarada", ex3);
         expect(typeof ex3).toBe("number");
         expect(Number.isInteger(ex3)).toBeTruthy();
+    });
+    it("Variável do tipo undefined", () => {
+        console.log("Variável declarada", ex4);
+        expect(typeof ex4).toBe("undefined");
     });
 });
