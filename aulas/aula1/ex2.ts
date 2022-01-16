e2b5ba446af0df86676e060f5bc286b444020f8c
Samuel Cardoso <samuelcardosodf@gmail.com>
2022-01-16 10:48:18 -0300

    incluido ex variavel tipo boolean

---
+++ aulas/aula1/ex2.ts
@@ -0,0 +1,13 @@
+/*
+ * Declarando variáveis e entendendo tipos de dados
+ * No escopo abaixo existe o código:
+ * "export default {}"
+ * No contexto atual estamos exportando por padrão o que vier na frente,
+ * no caso {}.
+ *
+ * Faça a declaração de uma variável do tipo boolean definida como falsa
+ * Troque "{}" pelo nome dado a variável
+ *
+ */
+
+export default {};
--- tests/aula1.spec.ts
+++ tests/aula1.spec.ts
@@ -1,8 +1,15 @@
 import ex1 from "../aulas/aula1/ex1";
+import ex2 from "../aulas/aula1/ex2";
 
 describe("Aula 1 - Conhecendo variáveis e seus tipos", () => {
     it("Variável do tipo string", () => {
+        console.log("Variável declarada", ex1);
         expect(typeof ex1).toBe("string");
+        expect(ex1.length).toBeGreaterThan(3);
+    });
+    it("Variável do tipo boolean", () => {
+        console.log("Variável declarada", ex2);
+        expect(typeof ex2).toBe("boolean");
         expect(ex1.length).toBeGreaterThan(3);
     });
 });
