2230ead42a16d70a6bf1df1750e92c1df4afa80a
Samuel Cardoso <samuelcardosodf@gmail.com>
2022-01-16 10:55:05 -0300

    incluido ex. variavel tipo number

---
+++ aulas/aula1/ex3.ts
@@ -0,0 +1,14 @@
+/*
+ * Declarando variáveis e entendendo tipos de dados
+ * No escopo abaixo existe o código:
+ * "export default {}"
+ * No contexto atual estamos exportando por padrão o que vier na frente,
+ * no caso {}.
+ *
+ * Faça a declaração de uma variável do tipo number definida com a sua idade
+ * Troque "{}" pelo nome dado a variável
+ *
+ */
+let number = 3;
+
+export default number;
--- tests/aula1.spec.ts
+++ tests/aula1.spec.ts
@@ -1,5 +1,6 @@
 import ex1 from "../aulas/aula1/ex1";
 import ex2 from "../aulas/aula1/ex2";
+import ex3 from "../aulas/aula1/ex3";
 
 describe("Aula 1 - Conhecendo variáveis e seus tipos", () => {
     it("Variável do tipo string", () => {
@@ -10,6 +11,11 @@ describe("Aula 1 - Conhecendo variáveis e seus tipos", () => {
     it("Variável do tipo boolean", () => {
         console.log("Variável declarada", ex2);
         expect(typeof ex2).toBe("boolean");
-        expect(ex1.length).toBeGreaterThan(3);
+        expect(ex2).toBeFalsy();
+    });
+    it("Variável do tipo number", () => {
+        console.log("Variável declarada", ex3);
+        expect(typeof ex3).toBe("number");
+        expect(Number.isInteger(ex3)).toBeTruthy();
     });
 });
