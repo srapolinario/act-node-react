b2dc9b999c5a6192b1e7813d06bef64034e8a501
Samuel Cardoso <samuelcardosodf@gmail.com>
2022-01-16 11:08:39 -0300

    incluido ex. variavel tipo null

--- aulas/aula1/ex5.ts
+++ aulas/aula1/ex5.ts
@@ -5,7 +5,7 @@
  * No contexto atual estamos exportando por padrão o que vier na frente,
  * no caso {}.
  *
- * Faça a declaração de uma variável do tipo undefined
+ * Faça a declaração de uma variável do tipo null
  * Troque "{}" pelo nome dado a variável
  *
  */
--- tests/aula1.spec.ts
+++ tests/aula1.spec.ts
@@ -2,12 +2,13 @@ import ex1 from "../aulas/aula1/ex1";
 import ex2 from "../aulas/aula1/ex2";
 import ex3 from "../aulas/aula1/ex3";
 import ex4 from "../aulas/aula1/ex4";
+import ex5 from "../aulas/aula1/ex5";
 
 describe("Aula 1 - Conhecendo variáveis e seus tipos", () => {
     it("Variável do tipo string", () => {
         console.log("Variável declarada", ex1);
         expect(typeof ex1).toBe("string");
-        expect(ex1.length).toBeGreaterThan(3);
+        //expect(ex1.length).toBeGreaterThan(3);
     });
     it("Variável do tipo boolean", () => {
         console.log("Variável declarada", ex2);
@@ -22,5 +23,9 @@ describe("Aula 1 - Conhecendo variáveis e seus tipos", () => {
     it("Variável do tipo undefined", () => {
         console.log("Variável declarada", ex4);
         expect(typeof ex4).toBe("undefined");
+    });
+    it("Variável do tipo null", () => {
+        console.log("Variável declarada", ex5);
+        expect(ex5 === null).toBeTruthy();
     });
 });
